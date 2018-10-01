2f66234c9f6382a22f13fbfac7f39707ea870ad1
r01and3 <dimka.pronckin@gmail.com>
2018-10-01 23:10:08 +0300

    Practice0 update

--- pronkin_da/practice/practice0/main.c
+++ pronkin_da/practice/practice0/main.c
@@ -2,33 +2,34 @@
 #include <math.h>
 
 void main() {
-	float x1, x2, y1, y2, r1, r2;
-	printf("Введите x и y центра окружности и r радиус окружности");
+	float x1, x2, y1, y2, r1, r2, rc;
+	printf("Vvedite x и y centri okryznosti u r radius okryznosti");
 	scanf("%f %f %f", &x1, &y1, &r1);
 	scanf("%f %f %f", &x2, &y2, &r2);
-	if (sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) > fabs(r1 - r2) && sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) < r1 + r2)
+	rc = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	if (rc > fabs(r1 - r2) && rc < r1 + r2)
 	{
-		printf("Окружности пересекаются");
+		printf("Okryznosti peresekaytsi");
 		return;
 	}
-	if (sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) < fabs(r1 - r2))
+	if (rc < fabs(r1 - r2))
 	{
-		printf("Одна окружность лежит внутри другой");
+		printf("Odna okryznost lezit vnytri drygoy");
 		return;
 	}
-	if (sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) > r1 + r2)
+	if (rc > r1 + r2)
 	{
-		printf("Окружности лежат на расстоянии друг от друга и не пересекаются");
+		printf("Okryznosti lezat na rasstoinii dryg ot dryga u ne peresekaytsi");
 		return;
 	}
-	if (sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == r1 + r2)
+	if (rc == r1 + r2)
 	{
-		printf("Окружности касаются внешним образом");
+		printf("Okryznosti kasaytsi vneshnim obrazom");
 		return;
 	}
-	if (sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == r1 - r2)
+	if (rc == r1 - r2)
 	{
-		printf("Окружности касаются внутренним образом");
+		printf("Okryznosti kasaytsi vnytrennim obrazom");
 		return;
 	}
 }
