0107f78b2c6a0e738b45eaea4eef9130731b386b
r01and3 <dimka.pronckin@gmail.com>
2019-09-20 10:14:07 +0300

    Update

--- pronkin_da/Practice/Practice4/main.c
+++ pronkin_da/Practice/Practice4/main.c
@@ -2,80 +2,66 @@
 #include <stdlib.h>
 #include <time.h>
 
-char NAME[5][4] = { "meat", "milk", "salt", "fish", "eggs"};
-char BARCODE[5][4] = {"1254", "7789", "4790", "1515", "1313" };
-int NAMEP = 0;
-int BARCODEP = 0;
+char *NAME[] = { "meat", "milk", "eggs", "salt", "fish" };
+char *BARCODE[] = { "1010", "1313", "1435", "2456", "1198" };
+float Sale[5];
 float PRICE[5] = { 150.0f, 75.0f, 40.0f, 250.0f, 60.0f };
-int PRICEP = 0;
-float SALE = 0;
 float FULLPRICE = 0;
-struct PProduct 
+float SALE = 0;
+
+struct PProduct
 {
-	char Name[4];
+	char *Name;
 	float Price;
 	int Quantity;
 	float FullPrice;
-	char BarCode[4];
+	char *BarCode;
 };
 typedef struct PProduct Product;
-Product P[5] = {0};
 
-void in(char bc)
-{
+void create(Product *_p) {
 	int i;
-	srand((unsigned int)time(0));
-	if (bc > 9999 || bc < 1)
-	{
-		return;
+	for (i = 0; i < 5; i++) {
+		Product p;
+		p.Name = NAME[i];
+		p.BarCode = BARCODE[i];
+		p.Price = PRICE[i];
+		p.Quantity = 0;
+		p.FullPrice = 0;
+		_p[i] = p;
 	}
-	for (i = 0; i < NAMEP; i++)
-		if (strcmp(bc, NAME[i])) //strcmp
-		{
-			P[i].Quantity++;
-			P[i].FullPrice = P[i].FullPrice + P[i].Price - P[i].Price * ((rand() * (49) / RAND_MAX + 1) / 100.0f);
-			FULLPRICE += P[i].FullPrice;
-			SALE += P[i].Price * ((rand() * (49) / RAND_MAX + 1) / 100.0f);
-			return;
+}
+
+void addout(char *bc, Product *_p) {
+	int i;
+	for (i = 0; i < 5; i++) {
+		if (!(strcmp(bc, _p[i].BarCode))) {
+			_p[i].Quantity++;
+			_p[i].FullPrice = _p[i].FullPrice + (_p[i].Price*(1 - Sale[i]));
+			FULLPRICE += _p[i].FullPrice;
+			SALE += (_p[i].Price * Sale[i]);
+			printf("%s", _p[i].Name);
+			printf(" - %.2f - %d - %.2f\n", _p[i].Price, _p[i].Quantity, _p[i].FullPrice);
 		}
-	Product p;
-	strcpy(p.Name, NAME[NAMEP]);
-	NAMEP++;
-	p.Price = PRICE[PRICEP];
-	PRICEP++;
-	p.Quantity = 0;
-	p.FullPrice = 0;
-	strcpy(p.BarCode, BARCODE[BARCODEP]);
-	BARCODEP++;
-	P[NAMEP - 1] = p;
-	return;
+	}
 }
 
-void out(int bc) 
-{
-	int i, n;
-	for (i = 0; i < NAMEP; i++) if (P[i].BarCode == bc) break;
-	for (n = 0; n < 4; n++) printf("%c", P[i].Name[n]); // printf("%s", P[i].Name)
-	printf(" - %.2f - %d - %.2f\n", P[i].Price, P[i].Quantity, P[i].FullPrice);
-}
-
-
-void main()
-{
-	int i = 0;
-	for (i; i < 6; i++) in(i);
-	i = 1;
-	printf("Enter Barcode Product Code\n");
-	scanf("%d", &i);
-	do
-	{
-		in(i);
-		out(i);
+void main() {
+	int i;
+	char bc[4];
+	Product p[5];
+	srand((unsigned int)time(0));
+	for (i = 0; i < 5; i++) Sale[i] = ((rand() * (49) / RAND_MAX + 1) / 100.0f);
+	create(p);
+	printf("Start\n");
+	while (i != 0) {
 		printf("Enter Barcode Product Code\n");
+		scanf("%s", &bc);
+		addout(bc, p);
+		printf("Enter one to add a new product\n");
 		printf("If you want to exit, enter 0\n");
 		scanf("%d", &i);
-	}while (i != 0);
-	printf("\n");
+	}
 	printf("Full Sale - %.2f\n", SALE);
 	printf("Full Price - %.2f", FULLPRICE);
 }
